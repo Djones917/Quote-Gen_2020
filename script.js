791f7e6d4b73f626666289bebe5ce64c8fbc4b08
DereK <nightwing917@hotmail.com>
2020-08-21 19:50:56 -0600

    starting proxy for cors work around

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Connected!');
+// Get Quote From API
+async function getQuote() {
+   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+   try {
+       const response = await fetch(apiUrl);
+       const data = await response.json();
+       console.log(data);
+   } catch (error) {
+       console.log('No Quote!', error);
+   }
+}
+
+
+
+// On Load
+getQuote();
