d6dd2bcd680ef11a35f0fca5f178c4a1ff7de2b1
Derek <nightwing917@hotmail.com>
2020-08-22 19:17:21 -0600

    starting to set up proxy server

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Get Quote From API
 async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
    try {
        const response = await fetch(apiUrl);
