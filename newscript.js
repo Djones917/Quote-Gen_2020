6a31586a65df2a92c7b935d5fcf64de2fe1793b8
Derek <nightwing917@hotmail.com>
2022-02-03 19:52:24 -0700

    added api quotes

--- newscript.js
+++ newscript.js
@@ -3,6 +3,7 @@ async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
       const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
     } catch (error) {
       // Catch Error Here
     }
