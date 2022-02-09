c75f7d5b49dd04641c89d38a170e6ebe8f31b151
Derek <nightwing917@hotmail.com>
2022-02-08 20:16:39 -0700

    console logging quote test

--- newscript.js
+++ newscript.js
@@ -3,6 +3,7 @@ let apiQuotes = [];
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
 }
 
 // Get Quotes From API
