60be411187bdf14cf63c319bfe14e2fd52e1871a
Derek <nightwing917@hotmail.com>
2022-02-09 19:32:05 -0700

    quoted out for testing on quotes from api function

--- newscript.js
+++ newscript.js
@@ -7,16 +7,16 @@ function newQuote() {
 }
 
 // Get Quotes From API
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-      const response = await fetch(apiUrl);
-      apiQuotes = await response.json();
-      newQuote();
-    } catch (error) {
-      // Catch Error Here
-    }
-}
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//       const response = await fetch(apiUrl);
+//       apiQuotes = await response.json();
+//       newQuote();
+//     } catch (error) {
+//       // Catch Error Here
+//     }
+// }
 
 // On Load
 getQuotes();
